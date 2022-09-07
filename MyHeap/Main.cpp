c7222eab3bfb97254d6d00550ebabc89a55fbe1a
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2022-09-07 15:32:17 +0900

    Feat: 힙 정렬 실행
    Main.cpp 수정

--- MyHeap/Main.cpp
+++ MyHeap/Main.cpp
@@ -1,26 +1,18 @@
-#include <cstdio>
 #include "MyHeap.h"
+#include <iostream>
 
 int main()
 {
 	Heap heap;
-	heap.push(1);
-	heap.push(9);
-	heap.push(8);
-	heap.push(7);
-	heap.push(6);
-	heap.push(15);
-	heap.push(4);
-	heap.push(3);
-	heap.push(2);
 
-	printf("%d\n", heap.top());
+	for (int num : {7, 4, 12, 27, 11, 6, 31, 5, 0, 1})
+	{
+		heap.push(num);
+	}
 
-	heap.push(18);
-	printf("%d\n", heap.top());
-
-	heap.pop();
-	printf("%d\n", heap.top());
-
-	printf("%d\n", heap.size());
+	while (false == heap.empty())
+	{
+		std::cout << heap.top() << ' ';
+		heap.pop();
+	}
 }
